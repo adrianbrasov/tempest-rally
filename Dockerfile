d4d186ade2d1ad707603ac69f7fb7b4f0161aed3
Adrian Ioan Brasov <abrasov@abrasov-mbp.romania.luxoft.com>
2019-05-28 16:32:14 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM ubuntu
+
+RUN apt-get update && apt-get install --yes sudo python python-pip vim wget git git-core tar && \
+    pip install --upgrade pip 
+
+WORKDIR /tmp
+
+RUN wget https://github.com/openstack/rally/archive/1.5.1.tar.gz &&\
+    gzip -d /tmp/1.5.1.tar.gz && \
+    tar -xvf /tmp/1.5.1.tar && \
+    /tmp/rally-1.5.1/install_rally.sh --target /rally_inst &&\
+    git clone https://git.openstack.org/openstack/tempest &&\
+    pip install tempest &&\
+    /bin/bash -c " . /rally_inst/bin/activate; pip install rally-openstack; \
+    rally verify create-verifier --type tempest --name tempest-verifier --source https://github.com/openstack/tempest.git " 
+
+ADD data /data
---
+++ data/start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+name=$1
+file=$2
+pattern=$3
+
+
+#functions
+create_deployment() {
+    rally deployment create --name $name --filename $file 
+}
+
+verify_start() {
+    if [ -z $pattern ]
+      then 
+        uuid=`rally verify start --detailed| \
+        tee /dev/stderr | grep UUID |tail -1 | awk -F '[=)]' '{print $2}'`
+    else 
+        uuid=`rally verify start --pattern $pattern --detailed | \
+        tee /dev/stderr | grep UUID |tail -1 | awk -F '[=)]' '{print $2}'`
+    fi
+}
+
+# source rally
+. /rally_inst/bin/activate
+
+# create deploymment 
+create_deployment
+
+# source deployment 
+source ~/.rally/openrc
+
+# start verification
+verify_start
+
+#generate report
+rally verify report --to /output/raport.html --uuid $uuid --type html
